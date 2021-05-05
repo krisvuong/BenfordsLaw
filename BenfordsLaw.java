3b945c312e45f56b9f77ad6ec1d42fa2da5adb2b
krisvuong <user@no.local>
2021-05-05 11:39:43 -0400

    Remove code
    Committed to wrong branch

--- BenfordsLaw.java
+++ BenfordsLaw.java
@@ -11,19 +11,3 @@ class BenfordsLaw{
 }
 
 //add methods here
-
-/**
- * Print the main menu
- * 
- * @author - Kris Vuong
- */
-public static void printMenu(){
-  Scanner reader = new Scanner(System.in);
-  
-  System.out.println("What would you like to do?");
-  System.out.println("[1] load \"sales.csv\"");
-  System.out.println("[2] check for fraud");
-  String choice = reader.nextLine();
-  
-  reader.close();
-}
