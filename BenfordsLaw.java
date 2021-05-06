b4d1c19f33f056215a55a114d075b630789565d5
krisvuong <user@no.local>
2021-05-06 11:28:59 -0400

    Rearrange method

--- BenfordsLaw.java
+++ BenfordsLaw.java
@@ -8,22 +8,22 @@ class BenfordsLaw{
   public static void main(String[] args){
     
   }
-}
 
-//add methods here
-
-/**
- * Print the main menu
- * 
- * @author - Kris Vuong
- */
-public static void printMenu(){
-  Scanner reader = new Scanner(System.in);
+  //add methods here
   
-  System.out.println("What would you like to do?");
-  System.out.println("[1] load \"sales.csv\"");
-  System.out.println("[2] check for fraud");
-  String choice = reader.nextLine();
-  
-  reader.close();
+  /**
+   * Print the main menu
+   * 
+   * @author - Kris Vuong
+   */
+  public static void printMenu(){
+    Scanner reader = new Scanner(System.in);
+    
+    System.out.println("What would you like to do?");
+    System.out.println("[1] load \"sales.csv\"");
+    System.out.println("[2] check for fraud");
+    String choice = reader.nextLine();
+    
+    reader.close();
+  }
 }
