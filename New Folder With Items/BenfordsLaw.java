65201d3084aa74bfb087c1cacf01d4ee7b0c8a7b
krisvuong <81175669+krisvuong@users.noreply.github.com>
2021-05-10 22:17:22 -0400

    Clean up code

--- New Folder With Items/BenfordsLaw.java
+++ New Folder With Items/BenfordsLaw.java
@@ -13,25 +13,6 @@ class BenfordsLaw{
     exportResults();
   }
 
-  
-//add methods here
-  
-  /**
-   * Print the main menu
-   * 
-   * @author - Kris Vuong
-   */
-  public static void printMenu(){
-    Scanner reader = new Scanner(System.in);
-    
-    System.out.println("What would you like to do?");
-    System.out.println("[1] load \"sales.csv\"");
-    System.out.println("[2] check for fraud");
-    String choice = reader.nextLine();
-    
-    reader.close();
-  }
-  
   /*
    * Export the digit frequency in a csv
    * 
@@ -39,8 +20,8 @@ class BenfordsLaw{
    * @param double[] freqArr - the frequency of each digit from 1-9
    */
   public static void exportResults() throws IOException{
-    //temp array
-    double[] temp = {29.10,32.94,10.67,4.05,5,6,7,8,9};
+    //temporary array
+    double[] per = {29.10,32.94,10.67,4.05,5,6,7,8,9};
     
     //Create new CSV file
     FileWriter fw = new FileWriter("DigitFrequency.csv");
@@ -49,7 +30,7 @@ class BenfordsLaw{
     pw.println("Digit, Frequency (%)");
     for(int i = 0; i<9; i++){
       pw.print(i+1 + ",");
-      pw.println(temp[i] + ",");
+      pw.println(per[i] + ",");
     }
     
     pw.close();
