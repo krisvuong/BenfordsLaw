405e1039601b1e0fd51fc851d26ee693a582cf80
krisvuong <user@no.local>
2021-05-10 21:22:56 -0400

    Finalize loadFile
    Returns a String containing validated directory of user-chosen file.

--- Folder/BenfordsLaw.java
+++ Folder/BenfordsLaw.java
@@ -5,11 +5,13 @@
  */
 import java.util.Scanner;
 import java.io.*;
-import java.net.*;
-import javax.swing.*;
+import javax.activation.MimetypesFileTypeMap;
+import java.net.URL;
+import java.net.URLConnection;
 
 class BenfordsLaw{
   public static void main(String[] args) throws IOException{
+    //System.out.println(loadFile());
     loadFile();
     
   System.out.println("Program terminated");
@@ -19,19 +21,35 @@ class BenfordsLaw{
    * Load input file through user input
    * 
    * @author - Kris Vuong
+   * @return String fileDir - String containing directory of user-selected sales file
    */
-  public static void loadFile() throws IOException{
-    JFileChooser j = new JFileChooser();
+  public static String loadFile() throws IOException{
+    Scanner reader = new Scanner(System.in);
     
-    String fileName = "sales.csv";
-    File salesData = new File(fileName);
-    Scanner reader = new Scanner(salesData);
-    /*
-    while(reader.hasNextLine()){
-      String line = reader.nextLine();
-      String sale = line.substring(line.indexOf(',')+1, line.length());
+    //Declare necessary variables
+    String fileDir; //user-inputted directory of sales file
+    boolean exists = false;  //if user-inputted file exists
+    File temp;  //file containing sales data
+    
+    //Loop until valid directory is entered
+    while(exists == false){
       
-      System.out.println(sale);
-    }*/
+      //Prompt file directory
+      System.out.println("Enter the sales file directory:");
+      fileDir = reader.nextLine();
+      
+      //Create instance of user-selected file
+      temp = new File(fileDir);
+      
+      //Check if the file exists
+      exists = temp.exists();
+      if(exists==true){
+        return fileDir;  //Return file directory
+      }
+      else{  //Reprompt if file/directory does not exist
+        System.out.println("This file/directory does not exist. Please try again.");
+      }
+    }
+    return "0";  //Arbitrary return value (will never occur)
   }
 }
